89ad51a692294a1c5975525a9511fd8d3c9bc623
crisc99 <52382957+crisc99@users.noreply.github.com>
2019-06-30 20:49:50 -0400

    Add files via upload

---
+++ Assets/changename.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changename : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            var textmeshPro = GetComponent<TMPro.TextMeshProUGUI>();
+        textmeshPro.SetText("Hello World from Cristian Cruz"); 
+        }
+    }
+}
